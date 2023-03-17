3ed41df1abf0c879e04fc7865f53a50386136afc
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-17 13:21:03 -0500

    1 file modified

--- 30DayOfPython/Day1/level1.py
+++ 30DayOfPython/Day1/level1.py
@@ -12,3 +12,14 @@ python --version
 "degu mulusew"
 "1000123"
 "I am enjoying 30 days of python"
+
+
+
+type(10)
+type((9.8))
+type(3.14)
+type(4 -4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type("mastewal")
+type("degu")
+type("Ethiopia")
