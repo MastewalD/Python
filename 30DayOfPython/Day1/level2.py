ba0a34e130217b65afeb41e10a31143341e4de95
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-17 13:36:48 -0500

    1 file modified

---
+++ 30DayOfPython/Day1/level2.py
@@ -0,0 +1,23 @@
+print(4+3)
+print(4-3)
+print(4*3)
+print(4%3)
+print(4**3)
+print(4/3)
+print(4//3)
+
+
+print("mastewaldegu")
+print("deu mulusew")
+print("Ethiopia")
+print("I am enjoying 30 days of python")
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 -4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("mastewal"))
+print(type("degu"))
+print(type("Ethiopia"))
