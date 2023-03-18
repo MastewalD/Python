98a25b455d18854a608bba95edea9bafef91333a
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-18 02:32:04 -0500

    2 files modified, 1 deleted

---
+++ 30DayOfPython/Day_2/level2.py
@@ -0,0 +1,16 @@
+first_name="mastewal"
+last_name="degu"
+full_name="mastewal degu"
+country="Ethiopia"
+city="addis ababa"
+age=23
+year=2023
+is_married=False
+is_true=True
+is_light_on= True
+hobby,favorite_movie,is_admin="movie","good will hunting",False
+
+print(type(first_name),type(last_name),type(full_name),type(country),type(city),type(age),type(year),type(is_married),type(is_true),type(is_light_on),type(hobby),type(favorite_movie),type(is_admin))
+
+
+print(len(first_name))
