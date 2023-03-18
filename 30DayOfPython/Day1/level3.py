522567f700375c34bcfe41950517845f22972d73
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-18 02:10:15 -0500

    1 file modified

--- 30DayOfPython/Day1/level3.py
+++ 30DayOfPython/Day1/level3.py
@@ -10,3 +10,15 @@ is_admin # boolean
     "name":"mastewal"
     "is_married": false  #dictionary
 }
+
+
+
+import math
+point1=(2,3)
+point2=(10,8)
+DIFFx=10-2
+DIFFy=8-3
+SQRx=DIFFx **2
+SQRy=DIFFy **2
+sum=SQRx +SQRy
+print(math.sqrt(sum))
