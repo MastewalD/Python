db58f607e41d159710129cb549da0214ee45cb8d
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-18 03:41:32 -0500

    1 file modified

--- 30DayOfPython/Day_3/exercise.py
+++ 30DayOfPython/Day_3/exercise.py
@@ -3,3 +3,9 @@ height=1.73
 complex=2 +2j
 
 print(type(complex))
+
+
+base=input("Enter base ")
+height=input("Enter height ")
+area_of_rectangle=int(base) *int(height)
+print("The area of the triangle is ",area_of_rectangle)
