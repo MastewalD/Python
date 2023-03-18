c6004b2b11448795ab0a26d085cae8aa2f21b5ef
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-18 01:53:05 -0500

    1 file modified

---
+++ 30DayOfPython/Day1/level3.py
@@ -0,0 +1,12 @@
+2 # int
+9.8 # float
+2+2j #complex
+"hello world"
+is_admin # boolean
+[1,2,3] #list
+(1,2,3) #tuple
+{1,2,3} #set
+{
+    "name":"mastewal"
+    "is_married": false  #dictionary
+}
