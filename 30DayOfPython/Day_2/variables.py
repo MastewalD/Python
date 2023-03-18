75985ee0662eefd12bb48d0931df748843bc7542
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-18 02:22:37 -0500

    4 files added

---
+++ 30DayOfPython/Day_2/variables.py
@@ -0,0 +1,12 @@
+# Day 2:30 Days of python programming
+first_name="mastewal"
+last_name="degu"
+full_name="mastewal degu"
+country="Ethiopia"
+city="addis ababa"
+age=23
+year=2023
+is_married=False
+is_true=True
+is_light_on= True
+hobby,favorite_movie,is_admin="movie","good will hunting",False
