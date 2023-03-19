e9a1a8eb4b0f563f88bc6836bfa8565f0c85f278
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-19 11:52:16 -0500

    1 file modified

--- 30DayOfPython/Day_3/exercise.py
+++ 30DayOfPython/Day_3/exercise.py
@@ -10,3 +10,11 @@ base=input("Enter base ")
 height=input("Enter height ")
 area_of_rectangle=int(base) *int(height)
 print("The area of the triangle is ",area_of_rectangle)
+
+
+
+a=input("Enter side a:")
+b=input("Enter side b:")
+c=input("Enter side c:")
+perimeter =int(a) +int(b)+int(c)
+print("The perimeter of the triangle is ", perimeter)
