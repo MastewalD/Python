e10a6ddf3f53bbf974005c3a2e67b010838c457b
mastewal degu <mastewaldegu0@gmail.com>
2023-03-22 19:21:26 +0300

    first commit

---
+++ 30DayOfPython/Day_4/Exercise.py
@@ -0,0 +1,15 @@
+import math
+"""
+print("Thirty "+"Days "+"Of "+"Python")
+
+print("Coding "+"For "+"All")
+"""
+company="Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
