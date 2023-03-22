631a71a8dbd5046f7d61fba75c7aa50d928c34f2
mastewal degu <mastewaldegu0@gmail.com>
2023-03-22 20:23:19 +0300

    second commit

--- 30DayOfPython/Day_4/Exercise.py
+++ 30DayOfPython/Day_4/Exercise.py
@@ -12,4 +12,32 @@ print(company.lower())
 print(company.capitalize())
 print(company.title())
 print(company.swapcase())
+print(company[7:])
+print(company.index("Coding"))
+print(company.find("Coding"))
+print(company.replace("Coding","Python"))
+str="Python for Everyone "
+str2=company.replace("Coding","Python")
+print(str.replace(str,str2))
+
+print(company.split())
+str3="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(str3.split(","))
+str4=company.split()
+print(str4[0])
+print(str4[len(str4)-1])
+
+print(company[10])
+
+
+
+
+
+
+
+
+
+
+
+
 
