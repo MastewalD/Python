5738c0d91f66ab46f173a7f88761c590c31f6237
mastewal degu <mastewaldegu0@gmail.com>
2023-03-22 21:07:02 +0300

    second commit

--- 30DayOfPython/Day_4/Exercise.py
+++ 30DayOfPython/Day_4/Exercise.py
@@ -28,6 +28,32 @@ print(str4[0])
 print(str4[len(str4)-1])
 
 print(company[10])
+print(company.index("C"))
+print(company.index("F"))
+print(company.rindex("l"))
+sentence= 'You cannot end a sentence with because because because is a conjunction'
+print(sentence.index("because"))
+print(sentence.rindex("because"))
+sentence1=sentence[31:54]
+print((sentence1))
+sentence2='You cannot end a sentence with because because because is a conjunction'
+print(sentence2.index("because"))
+print(company.startswith("Coding"))
+print(company.endswith("coding"))
+sentence5="30DaysOfPython"
+sentence6="thirty_days_of_python"
+print(sentence6.isidentifier())
+print(sentence5.isidentifier())
+frame_work=["Django","Flask","Bottle","Pyramid","Falcon"]
+print(" ".join(frame_work))
+sentence7="I am enjoying this challenge.\nI just wonder what is next."
+print(sentence7)
+sentence8="Name\tAge \tCountry \tcity\nAsabeneh\t250 \tFinland \tHelsinki"
+print(sentence8)
+
+
+
+
 
 
 
