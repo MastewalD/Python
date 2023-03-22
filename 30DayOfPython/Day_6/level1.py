0e04f4a68d04cca5d4dd616e68090185137f7740
mastewal degu <mastewaldegu0@gmail.com>
2023-03-22 21:38:59 +0300

    first commit

---
+++ 30DayOfPython/Day_6/level1.py
@@ -0,0 +1,11 @@
+tpl=()
+print(tpl)
+print(type(tpl))
+sisters=("asi","tutu","mita")
+brothers=("dera","lul")
+sibiling=sisters + brothers
+print(sibiling)
+print(len(sibiling))
+parent=("aba","eme")
+family_member=sibiling+parent
+print(family_member)
