447e9d834a94049fc8bee29b38446da4261804c5
mastewal degu <mastewaldegu0@gmail.com>
2023-03-22 23:29:06 +0300

    first commit

---
+++ 30DayOfPython/Day_7/level1.py
@@ -0,0 +1,12 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+
+print(len(it_companies))
+it_companies_ls=list(it_companies)
+it_companies_ls[0]="Twitter"
+it_companies_st=set(it_companies_ls)
+print(it_companies_st)
