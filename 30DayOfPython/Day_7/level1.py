945eec61604396a075d5bdebf5d78b5350058393
mastewal degu <mastewaldegu0@gmail.com>
2023-03-23 10:08:59 +0300

    first commit

--- 30DayOfPython/Day_7/level1.py
+++ 30DayOfPython/Day_7/level1.py
@@ -10,3 +10,21 @@ it_companies_ls=list(it_companies)
 it_companies_ls[0]="Twitter"
 it_companies_st=set(it_companies_ls)
 print(it_companies_st)
+it_companies_st.update(["company1","company2","company3"])
+print(it_companies_st)
+it_companies_st.remove("company1")
+print(it_companies_st)
+print(A.union(B))
+print(A.intersection(B))
+print(A.issubset(B))
+print(A.isdisjoint(B))
+print(A.union(B),B.union(A))
+print(A.symmetric_difference(B))
+del A
+del B
+
+
+
+
+
+
