c1ecb0b1d005c72e235f7eda1d2dfd28996d8637
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-24 10:02:39 +0300

    3 files added

---
+++ 30DayOfPython/Day_9/level1.py
@@ -0,0 +1,44 @@
+import math
+
+age= input('Enter your age:')
+difference=18-int(age)
+Age=int(age)
+if Age >= 18:
+    print('You are old enough to learn to drive.')
+else:
+        print('You need', difference ,'more years to learn to drive.')
+        
+        
+
+
+        Your_Age=input('Enter your age:')
+difference=int(Your_Age) -25
+diff=abs(difference)
+if difference > 0:
+    print('You are', difference, 'years older than me.')
+else:
+        print('You are',diff,'years younger than me.')
+
+
+        number_one=input('Enter number one:')
+number_two=input('Enter number two:')
+if int(number_one) > int(number_two):
+    print(number_one,'is greater than ',number_two)
+    
+else:
+        print(number_one,'is less than ',number_two)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
